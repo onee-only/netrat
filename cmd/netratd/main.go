4e17b6123c20951d4ecae9c373b8dc28a91dc08d
onee-only <kimww0306@gmail.com>
2024-03-23 20:45:32 +0900

    Apply automaxprocs

--- cmd/netratd/main.go
+++ cmd/netratd/main.go
@@ -6,14 +6,21 @@ import (
 	"os"
 	"os/signal"
 
+	_ "go.uber.org/automaxprocs"
+
 	"github.com/onee-only/netrat/cmd/netratd/server"
+	"github.com/onee-only/netrat/internal/config"
 )
 
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), signalsToHandle...)
 	defer stop()
 
-	srv := server.New(server.Options{})
+	opts := server.Options{
+		SocketAddr: config.DefaultServerAddr,
+	}
+
+	srv := server.New(opts)
 
 	if err := srv.Run(ctx); err != nil {
 		fmt.Fprintf(os.Stderr, "netratd: %s\n", err)
