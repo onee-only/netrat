7a820d36eb62f1f2ea78680f322dfc987999efb2
onee-only <kimww0306@gmail.com>
2024-03-28 10:28:35 +0900

    Add more defaults

--- internal/config/defaults.go
+++ internal/config/defaults.go
@@ -2,4 +2,13 @@ package config
 
 const (
 	DefaultServerAddr = "/var/run/netrat.sock"
+	DefaultDataPath   = "/tmp"
+)
+
+const (
+	PacketSnapLen int32 = 65535
+)
+
+const (
+	PacketStreamBufSize = 10
 )
